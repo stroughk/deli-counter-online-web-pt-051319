6a93cc67676e01c2abc8e1a15296942e7d0e7283
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 18:43:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,11 @@
 def line(katz_deli)
+    
   if katz_deli.empty? == true
     puts "The line is currently empty."
+    elsif katz_deli.empty? == false 
+    katz_deli.each_with_index do |name, index|
+    puts "The line is currently: #{index}. #{name}"
+  end 
   end 
   
 end 
